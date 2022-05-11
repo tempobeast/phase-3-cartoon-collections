e720c370054a142f1b538c88026832a1e2eeb059
Zach <shaw.zacht@gmail.com>
2022-05-11 16:36:13 -0700

    commit tree 37fa4e2841d3e228b55faae5aeac09c35b3c2e74

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.map.with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteers
+  planeteers.map do |planeteer|
+    planeteer[0].upcase.concat(planeteer.slice(1, planeteer.length)) << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese snacks
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |snack|
+    if cheese_types.include? (snack)
+      snack
+    else
+      nil
+    end
+  end
 end
